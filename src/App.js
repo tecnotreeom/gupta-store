ab3d0a4187ed5fd7563ebc5c98e5acea30dee058
guptaom <omprakash.gupta@tecnotree.com>
2020-04-27 03:55:00 +0530

    update our menu yiem iyems to navigate  with router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import  HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
+const HatPage = () => (
+  <div>
+    <h3> HATS PAGE</h3>
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage />
+        <Switch>
+        <Route exact path = "/" component = {HomePage} />
+        <Route exact path = "/hats" component = {HatPage} />
+        </Switch>
     </div>
   );
 }
