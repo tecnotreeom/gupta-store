5d24bd7398cf3e16ccbfb6c23dd5c015cf2911f1
guptaom <omprakash.gupta@tecnotree.com>
2020-04-26 17:43:20 +0530

    creaed project with create react app and adding file for hompage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './homepage-styles.scss';
+
+  const HomePage = () => (
+    <div className = "homepage">
+        <div className = "directory-menu">
+            <div className = "menu-item">
+                <div className = "content">
+                    <h2 className = "title">Threads</h2>
+                    <span className = "subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className = "menu-item">
+                <div className = "content">
+                    <h2 className = "title">BUttons</h2>
+                    <span className = "subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className = "menu-item">
+                <div className = "content">
+                    <h2 className = "title">Machine Part</h2>
+                    <span className = "subtitle">SHOP NOW</span>
+                </div>
+            </div>
+
+            <div className = "menu-item">
+                <div className = "content">
+                    <h2 className = "title">Cloths</h2>
+                    <span className = "subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className = "menu-item">
+                <div className = "content">
+                    <h2 className = "title">Cloths</h2>
+                    <span className = "subtitle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className = "menu-item">
+                <div className = "content">
+                    <h2 className = "title">Cloths</h2>
+                    <span className = "subtitle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+export default HomePage;
