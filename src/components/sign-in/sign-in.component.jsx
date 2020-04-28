102259c3deeebcd310687d849386569c316df233
guptaom <omprakash.gupta@tecnotree.com>
2020-04-28 21:57:44 +0530

    sign in for old user

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import FormInput from '../form-input/form-input.component.';
 
 import CustomButton from '../custom-button/custom-button..component';
 
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
 
@@ -17,9 +17,16 @@ class SignIn extends React.Component {
             password: ''
         }
     }
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email:'', password:''})
+        const {email, password} = this.state;
+        try {
+            await auth.createUserWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''})
+        }catch(error) {
+            console.log(error);
+        }
+        
     }
     handleChange = event => {
         const {value, name} = event.target;
